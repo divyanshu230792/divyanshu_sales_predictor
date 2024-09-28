9e12ca648122056cdd58fa7239dae53a93909124
divyanshu230792 <divyanshusharma0005@gmail.com>
2024-09-28 20:01:44 +0530

    csvfile path

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
+
 MODEL_FILENAME=r'artifacts/sales_model1.pickle'
 COLUMN_DATA=r'artifacts/sale_model_col_data1.json'
-CSV_FILENAME=r'data/Dummy Data HSS.csv'
+CSV_FILENAME=r'data\Dummy Data HSS.csv'
 FLASK_PORT_NO=8082
